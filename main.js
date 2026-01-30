1bf7e97ae34c5f8a6eda43862c3374a90944f974
Franco <cardenas.franco.935@gmail.com>
2026-01-29 23:42:27 -0300

    Agrega menu principal

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
-console.log("Gestor de gastos iniciado");
-let gasto = 0;
+function menu() {
+  let numero = parseInt(prompt("Ingresa un numero"));
+  let continuar = true;
+  while (continuar === true) {
+    switch (numero) {
+      case 1:
+        agregarGastos();
+        break;
+      case 2:
+        listarGastos();
+        break;
+      case 3:
+        calcularTotal();
+        break;
+      case 3:
+        eliminarGastos();
+      case 4:
+        continuar = false;
+        break;
+    }
+  }
+}
+menu();
